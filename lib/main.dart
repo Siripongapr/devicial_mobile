bb2e9c25e8e45f7f6ea6e6a0e4713156524bcac9
Siripongapr <siripong.a@tconio.com>
2024-04-22 06:22:10 +0700

    fix : remove cookie when open app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:devicial_mobile/blocs/login/login_cubit.dart';
+import 'package:devicial_mobile/blocs/register/register_cubit.dart';
 import 'package:devicial_mobile/materials/color.dart';
 import 'package:devicial_mobile/repository/auth.dart';
 import 'package:devicial_mobile/router.dart';
 import 'package:devicial_mobile/screen/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Get the application documents directory
+  var appDocDir = await getApplicationDocumentsDirectory();
+  var cookiePath = '${appDocDir.path}/cookies';
+
+  // Initialize the cookie jar with the storage path
+  var cookieJar = PersistCookieJar(storage: FileStorage(cookiePath));
+
+  // Clear all cookies
+  await cookieJar.deleteAll();
+
   runApp(const MyApp());
 }
 
@@ -20,6 +35,7 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<LoginCubit>(create: (context) => LoginCubit(Auth())),
         BlocProvider<LogoutCubit>(create: (context) => LogoutCubit(Auth())),
+        BlocProvider<RegisterCubit>(create: (context) => RegisterCubit(Auth())),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
